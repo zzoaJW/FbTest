93fd73e5de5fed801be1b783a0d061cdd4177dd8
zzoaJW <i7can7i@gmail.com>
2023-01-05 03:16:40 +0900

    Functional Implementation : Login & Register

--- app/src/main/java/com/z0o0a/fbtest/UserPage.kt
+++ app/src/main/java/com/z0o0a/fbtest/UserPage.kt
@@ -1,7 +1,10 @@
 package com.z0o0a.fbtest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.z0o0a.fbtest.databinding.UserPageBinding
 
 class UserPage : AppCompatActivity() {
@@ -13,6 +16,20 @@ class UserPage : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val user = Firebase.auth.currentUser
 
+        binding.txtUserInfo.text = user!!.email
+
+        binding.btnSaveDb.setOnClickListener {
+            val input1 = binding.edittextName.text
+            val input2 = binding.edittextAge.text
+            val input3 = binding.edittextDesc.text
+        }
+
+        binding.btnLogout.setOnClickListener {
+            Firebase.auth.signOut()
+            Toast.makeText(this, "로그아웃", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
